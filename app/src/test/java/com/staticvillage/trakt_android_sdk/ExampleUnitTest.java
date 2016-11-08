ed0073830ecfd8716b7c28c02c9d8f9c7818abc1
joelparrish <joel@stashinvest.com>
2016-11-07 23:06:49 -0500

    initial commit

--- app/src/test/java/com/staticvillage/trakt_android_sdk/ExampleUnitTest.java
+++ app/src/test/java/com/staticvillage/trakt_android_sdk/ExampleUnitTest.java
@@ -2,7 +2,7 @@ package com.staticvillage.trakt_android_sdk;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
