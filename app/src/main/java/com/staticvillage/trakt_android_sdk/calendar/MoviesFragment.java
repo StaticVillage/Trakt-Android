8585a68c266242f568c079f777edcf21b4f25ef4
joelparrish <jpdev832@gmail.com>
2016-11-08 23:55:56 -0500

    Add date and days selection in calendar

--- app/src/main/java/com/staticvillage/trakt_android_sdk/calendar/MoviesFragment.java
+++ app/src/main/java/com/staticvillage/trakt_android_sdk/calendar/MoviesFragment.java
@@ -1,9 +1,9 @@
 package com.staticvillage.trakt_android_sdk.calendar;
 
 import com.staticvillage.trakt_android_sdk.App;
-import com.staticvillage.trakt_android_sdk.common.ExtendedResultFragment;
+import com.staticvillage.trakt_android_sdk.common.ExtendedCalendarResultFragment;
 
-import java.util.Calendar;
+import java.util.Date;
 
 import rx.Observable;
 
@@ -11,16 +11,24 @@ import rx.Observable;
  * Created by joelparrish on 11/6/16.
  */
 
-public class MoviesFragment extends ExtendedResultFragment {
+public class MoviesFragment extends ExtendedCalendarResultFragment {
     public static MoviesFragment newInstance() {
         return new MoviesFragment();
     }
 
     @Override
+    public void setUrl() {
+        endpointUrl.setText("calendars/all/movies/{start_date}/{days}");
+    }
+
+    @Override
     public Observable<String> getResult() {
+        Date startDate = getStartDate();
+        int days = getDays();
+
         boolean extended = extendedCheckBox.isChecked();
         return App.getTraktService()
-                .getMovies(Calendar.getInstance().getTime(), 7, extended, null)
+                .getMovies(startDate, days, extended, null)
                 .map(response -> gson.toJson(response));
     }
 }
--- app/src/main/java/com/staticvillage/trakt_android_sdk/calendar/NewShowsFragment.java
+++ app/src/main/java/com/staticvillage/trakt_android_sdk/calendar/NewShowsFragment.java
@@ -1,9 +1,9 @@
 package com.staticvillage.trakt_android_sdk.calendar;
 
 import com.staticvillage.trakt_android_sdk.App;
-import com.staticvillage.trakt_android_sdk.common.ExtendedResultFragment;
+import com.staticvillage.trakt_android_sdk.common.ExtendedCalendarResultFragment;
 
-import java.util.Calendar;
+import java.util.Date;
 
 import rx.Observable;
 
@@ -11,16 +11,23 @@ import rx.Observable;
  * Created by joelparrish on 11/6/16.
  */
 
-public class NewShowsFragment extends ExtendedResultFragment {
+public class NewShowsFragment extends ExtendedCalendarResultFragment {
     public static NewShowsFragment newInstance() {
         return new NewShowsFragment();
     }
 
     @Override
+    public void setUrl() {
+        endpointUrl.setText("calendars/all/shows/new/{start_date}/{days}");
+    }
+
+    @Override
     public Observable<String> getResult() {
+        Date startDate = getStartDate();
+        int days = getDays();
         boolean extended = extendedCheckBox.isChecked();
         return App.getTraktService()
-                .getNewShows(Calendar.getInstance().getTime(), 7, extended, null)
+                .getNewShows(startDate, days, extended, null)
                 .map(response -> gson.toJson(response));
     }
 }
